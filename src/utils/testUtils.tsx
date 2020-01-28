e30c9e5418fc5e9f1396f02c2a4d639acd50efa8
Prashant Goel <prashantmanunited@gmail.com>
2020-01-28 16:21:14 +0530

    feat: add additional route argument to renderWithRouter test utils

--- src/utils/testUtils.tsx
+++ src/utils/testUtils.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import { createMemoryHistory, MemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
 export const renderWithRouter = (
   ui: React.ReactElement,
   {
+    path = '/',
     route = '/',
     history = createMemoryHistory({ initialEntries: [route] }),
-  }: { route?: string; history?: MemoryHistory } = {},
+  }: { path?: string; route?: string; history?: MemoryHistory } = {},
 ) => ({
-  ...render(<Router history={history}>{ui}</Router>),
+  ...render(
+    <Router history={history}>
+      <Route
+        path={path}
+        render={routeProps => React.cloneElement(ui, { ...routeProps })}
+      />
+    </Router>,
+  ),
   history,
 });
